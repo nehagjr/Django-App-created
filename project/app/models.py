9e3c21c45691526ef68fe7802539d8fefb9237d1
nehagjr <pneha3169@gmail.com>
2024-07-15 18:32:44 +0530

    create model

--- project/app/models.py
+++ project/app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    Student_nm=models.CharField(max_length=20)
+    email=models.EmailField()
+    city=models.CharField(max_length=20)
+    mobile=models.IntegerField()
--- project/app/views.py
+++ project/app/views.py
@@ -3,3 +3,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
     return HttpResponse("<h1>Welcome to home  of app </h1>")
+ 
