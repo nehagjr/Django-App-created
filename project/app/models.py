af5f616d82e70f65661362065547d91d0bf82de9
nehagjr <pneha3169@gmail.com>
2024-07-16 18:29:54 +0530

    Work on admin pannel

--- project/app/models.py
+++ project/app/models.py
@@ -9,3 +9,13 @@ class Student(models.Model):
 
     def __str__(self):
         return self.Student_nm+' '+self.email
+    
+    class Meta:
+        db_table="StudentTbl"
+
+        #Admin panal pr s add kr k deta hai  ---> " Student1s "
+        # verbose_name="Student1"
+
+        # admin panal k name me baad me s nahi aayega ----->" Student "
+        verbose_name_plural="Student"
+        ordering=['Student_nm']
